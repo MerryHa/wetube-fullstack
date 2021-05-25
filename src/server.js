e141366084a51f4cc422a8c9c81068d9a4e4214e
Merry <wiostz98kr@gmail.com>
2021-05-25 14:54:39 +0900

    use router

--- src/server.js
+++ src/server.js
@@ -4,18 +4,16 @@ import morgan from "morgan";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
-
-
-const home = (req, res, next) => {
-    console.log("I will respond.");
-    return res.send("hello.");
-};
-const login = (req, res) => {
-    return res.send("Login");
-}
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videoRouter = express.Router();
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/user", userRouter);
+
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}!`);
 
