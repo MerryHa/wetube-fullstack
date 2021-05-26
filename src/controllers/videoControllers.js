e05e03aef176e94944661236fef28940a6c3b96e
MerryHa <wiostz98kr@gmail.com>
2021-05-26 23:36:09 +0900

    use variables to templates

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,4 +1,4 @@
-export const trending = (req, res) => res.render("home"); //렌더할 뷰 이름을 넣어준다.
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", name: "Noma" });
 export const see = (req, res) => res.render("watch");
 
 export const edit = (req, res) => res.render("edit");
