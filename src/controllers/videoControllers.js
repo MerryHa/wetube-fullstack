dd9d7d93795c088491329d0a35700f4e5405c8ea
MerryHa <wiostz98kr@gmail.com>
2021-06-01 09:46:43 +0900

    separate init.js from server.js

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,48 +1,27 @@
-const videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 2,
-    },
-    {
-        title: "Third Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 3,
-    },
-];
-export const trending = (req, res) => {
-    return res.render("home", { pageTitle: "Home", videos });
-}
+import Video from '../models/Video';
+
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({});
+        return res.render("home", { pageTitle: "Home", videos });
+    } catch {
+        return res.render("server-error");
+    }
+};
+
 export const watch = (req, res) => {
     //const id = req.params.id;
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+    return res.render("watch", { pageTitle: `Watching` });
 }
 
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+    return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`); //브라우저가 자동으로 우리가 준 url로 이동하게 하는 것
 }
 export const getUpload = (req, res) => {
@@ -50,15 +29,6 @@ export const getUpload = (req, res) => {
 }
 export const postUpload = (req, res) => {
     const { title } = req.body;
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        views: 0,
-        id: videos.length + 1,
-    }
-    videos.push(newVideo);
     return res.redirect("/");
 }
 
