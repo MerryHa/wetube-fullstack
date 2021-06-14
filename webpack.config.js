667bdc95fe5cd1a6a1b5c3b1eab7ecc7d5cdadf4
MerryHa <wiostz98kr@gmail.com>
2021-06-14 17:08:51 +0900

    set SCSS loader and MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
     mode: "development",
     entry: './src/client/js/main.js',
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/styles.css",
+    })],
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'assets', "js"),
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'assets'),
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
+                //loader를 사용하는 첫 번째 방법
                 use: {
                     loader: "babel-loader",//node-modules에서 babel-loader 찾음
                     options: {//해당 로더에게 다음과 같은 옵션 전달
                         presets: [["@babel/preset-env", { targets: "defaults" }]],
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                //loader를 사용하는 두 번째 방법
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
     }
