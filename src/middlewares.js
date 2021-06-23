2e05a07734d0f43bb5e4de3971b777699a197d10
MerryHa <wiostz98kr@gmail.com>
2021-06-23 17:02:00 +0900

    add isHeroku variable

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,20 +2,29 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import aws from "aws-sdk";
 
+const isHeroku = process.env.NODE_ENV === "production";
+
 const s3 = new aws.S3({
     credentials: {
         accessKeyId: process.env.AWS_ID,
         secretAccessKey: process.env.AWS_SECRET,
     }
 })
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
     s3: s3,
-    bucket: 'nomatube',
+    bucket: 'nomatube/images',
+    acl: 'public-read'
+})
+const s3VideoUploader = multerS3({
+    s3: s3,
+    bucket: 'nomatube/videos',
     acl: 'public-read'
 })
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.loggedInUser = req.session.user || {};
+    res.locals.siteName = "Wetube";
+    res.locals.isHeroku = isHeroku;
     next();
 };
 
@@ -40,12 +49,12 @@ export const avatarUploadMiddleware = multer({
     limits: {
         fileSize: 3000000
     },
-    storage: multerUploader
+    storage: isHeroku ? s3ImageUploader : undefined
 }) // byte단위
 export const videoUploadMiddleware = multer({
     dest: "uploads/videos/",
     limits: {
-        fileSize: 100000000
+        fileSize: 10000000
     },
-    storage: multerUploader
+    storage: isHeroku ? s3VideoUploader : undefined
 })
