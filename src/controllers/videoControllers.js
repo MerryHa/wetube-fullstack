8b0d51507a7c2308986acf3679338ca5a7c943a1
Noma <wiostz98kr@gmail.com>
2021-06-30 22:07:54 +0900

    Bug Fix - about video edit permission

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -45,7 +45,7 @@ export const postEdit = async (req, res) => {
         body: { description, title, hashtags }
 
     } = req;
-    const video = await Video.exists({ _id: id });
+    const video = await Video.findOne({ _id: id });
     if (!video) {
         return res.status(404).render("404", { pageTitle: "Video not found." });
     }
